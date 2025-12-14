62b3a0b7e18cbcf50772ea6e856b1b07c8bb15fd
ivanobivan <ikolesov2015@gmail.com>
2025-12-14 23:18:48 +0300

    Update new-base.sh

--- sh/hands/system/new-base.sh
+++ sh/hands/system/new-base.sh
@@ -3,3 +3,7 @@
 #fzf install script
 #git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 #~/.fzf/install
+
+# rg for debian
+#$ curl -LO https://github.com/BurntSushi/ripgrep/releases/download/14.1.1/ripgrep_14.1.1-1_amd64.deb
+#$ sudo dpkg -i ripgrep_14.1.1-1_amd64.deb
