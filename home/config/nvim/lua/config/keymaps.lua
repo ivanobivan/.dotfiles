103848ca251a96f0d64af6ebfaed906f1109908d
ivanobivan <ikolesov2015@gmail.com>
2025-12-24 16:03:29 +0300

    feat: new keymaps for navigations

--- home/config/nvim/lua/config/keymaps.lua
+++ home/config/nvim/lua/config/keymaps.lua
@@ -1,1 +1,4 @@
+local map = vim.keymap.set
 
+map("n", "<A-Left>", "<C-o>", { noremap = true, silent = true, desc = "Go back in jump list" })
+map("n", "<A-Right>", "<C-i>", { noremap = true, silent = true, desc = "Go forward in jump list" })
