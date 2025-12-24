886114cec10f4e4ebbca8d7217b3e127e63210c6
ivanobivan <ikolesov2015@gmail.com>
2025-12-24 14:28:43 +0300

    feat: new keymaps for copy filename and filepath

--- home/config/nvim/lua/plugins/snacks.lua
+++ home/config/nvim/lua/plugins/snacks.lua
@@ -32,5 +32,25 @@ return {
                 timeout = 5000,
             },
         },
+        keys = {
+            {
+                "<leader>fn",
+                function()
+                    local file = vim.fn.expand("%:t")
+                    vim.fn.setreg("+", file)
+                    vim.notify("Copied file name: " .. file)
+                end,
+                desc = "Copy file name",
+            },
+            {
+                "<leader>fp",
+                function()
+                    local file = vim.fn.expand("%:p")
+                    vim.fn.setreg("+", file)
+                    vim.notify("Copied full path: " .. file)
+                end,
+                desc = "Copy full path",
+            },
+        },
     },
 }
