b4ea1913bf6a78ba6b71e242743781d52d8e3f07
ivanobivan <ikolesov2015@gmail.com>
2025-12-26 15:25:25 +0300

    feat: try jj as escape

--- home/config/nvim/lua/config/keymaps.lua
+++ home/config/nvim/lua/config/keymaps.lua
@@ -2,3 +2,5 @@ local map = vim.keymap.set
 
 map("n", "<A-Left>", "<C-o>", { noremap = true, silent = true, desc = "Go back in jump list" })
 map("n", "<A-Right>", "<C-i>", { noremap = true, silent = true, desc = "Go forward in jump list" })
+
+map("i", "jj", "<Esc>", { desc = "Exit insert mode" })
