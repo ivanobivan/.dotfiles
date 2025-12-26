3c85d7e445c785ee7724bcf00e1dbbbaca9ac5cf
ivanobivan <ikolesov2015@gmail.com>
2025-12-26 12:53:42 +0300

    feat: enable shift selection

--- home/config/nvim/lua/config/options.lua
+++ home/config/nvim/lua/config/options.lua
@@ -1,7 +1,12 @@
+-- disable animations
 vim.g.snacks_animate = false
 
--- отключаем recording
+-- disable recording
 vim.opt.shortmess:append("q")
 
+-- add breadcrumbs recording blank lines
 vim.opt.list = true
 vim.opt.listchars:append("space:⋅")
+
+-- add shift/ctrl selectin behavior in n x mode
+vim.opt.keymodel:append({ "startsel", "stopsel" })
