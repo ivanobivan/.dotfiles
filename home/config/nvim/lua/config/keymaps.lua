f9b9a7ed9c7491be48507d29c8ed767c7dc1c194
ivanobivan <ikolesov2015@gmail.com>
2025-12-29 17:57:24 +0300

    feat: remove arrow conf

--- home/config/nvim/lua/config/keymaps.lua
+++
@@ -1,4 +0,0 @@
-local map = vim.keymap.set
-
-map("n", "<A-Left>", "<C-o>", { noremap = true, silent = true, desc = "Go back in jump list" })
-map("n", "<A-Right>", "<C-i>", { noremap = true, silent = true, desc = "Go forward in jump list" })
