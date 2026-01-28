a9ff913304474456c92c0f10fb98620e38effa1e
ivanobivan <ikolesov2015@gmail.com>
2026-01-28 19:14:17 +0300

    feat(nvim): customize theme

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,11 +1,22 @@
 return {
-    {
-        "rebelot/kanagawa.nvim",
-        opts = {
-            theme = "wave",
-            background = {
-                dark = "wave",
-            },
+    "rebelot/kanagawa.nvim",
+    opts = {
+        theme = "wave",
+        background = {
+            dark = "wave",
         },
+        overrides = function(colors)
+            local theme = colors.theme
+            local bg = "#1c1f26"
+            return {
+                Normal = { fg = theme.ui.fg, bg = bg },
+                NormalFloat = { bg = bg },
+                FloatBorder = { bg = bg, fg = theme.ui.fg },
+                SignColumn = { bg = bg },
+                FoldColumn = { bg = bg },
+                LineNr = { bg = bg },
+                CursorLineNr = { bg = bg },
+            }
+        end,
     },
 }
